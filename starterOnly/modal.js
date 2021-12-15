9b381246ed575df5c8b5c58700dcd12d0d1f2b29
nicolaspatschkowski <47756052+nicolaspatschkowski@users.noreply.github.com>
2021-12-15 13:31:47 +0100

    Work in progress

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -10,7 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +19,33 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// close modal form when clicking on the X
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", closeModal);
+
+// When clicking outside of the form close modal form
+window.addEventListener("click", function (event) {
+    if (event.target == modalbg) {
+        modalbg.style.display = "none";
+    }
+});
+
+// close modal form when clicking on the ESC key
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        modalbg.style.display = "none";
+    }
+});
+
+// close modal form when clicking on the submit button
+const submitBtn = document.querySelector(".btn-submit");
+submitBtn.addEventListener("click", closeModal);
+
 
 
