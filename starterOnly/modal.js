e46b674a0af6889dc0c4a707b1377ff5998036b6
TRMHTT REQ <tommy.requillard@gmail.com>
2022-11-07 14:59:32 +0100

    Work in progress

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  let x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -25,27 +25,42 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// close modal form when clicking on the X
-const closeBtn = document.querySelector(".close");
-closeBtn.addEventListener("click", closeModal);
-
 // When clicking outside of the form close modal form
 window.addEventListener("click", function (event) {
-    if (event.target == modalbg) {
-        modalbg.style.display = "none";
+    if (event.target === modalbg) {
+        closeModal();
     }
 });
 
 // close modal form when clicking on the ESC key
 document.addEventListener("keydown", function (event) {
     if (event.key === "Escape") {
-        modalbg.style.display = "none";
+        closeModal();
     }
 });
 
-// close modal form when clicking on the submit button
-const submitBtn = document.querySelector(".btn-submit");
-submitBtn.addEventListener("click", closeModal);
 
 
+// stock data form
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
 
+
+// close modal form when clicking on the submit button if all the fields are filled in
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (firstName.value && lastName.value && email.value && birthdate.value && quantity.value && (location1.checked || location2.checked || location3.checked || location4.checked || location5.checked || location6.checked)) {
+        closeModal();
+    }
+}
+);
